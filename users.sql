fa79e51627b72d022ce03b15e8f74c6c6f3bbd50
rafael271011 <rafahl.iakovou@gmail.com>
2025-05-28 23:41:33 +0300

    Update users.sql

--- users.sql
+++ users.sql
@@ -11,33 +11,27 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `cmp210`
---
+
+CREATE DATABASE IF NOT EXISTS `cmp210` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cmp210`;
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
 (1, 'john_doe', 'john.doe@example.com', 'password123', '2025-05-21 15:53:44'),
@@ -47,25 +41,6 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
 (5, 'david_wilson', 'david.wilson@example.com', 'davidsSecret234', '2025-05-21 15:53:44'),
 (6, 'petros', 'petros.doe@example.com', 'passpass', '2025-05-21 16:03:33');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
